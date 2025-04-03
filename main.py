743590baffa808fa524393a38b30cd52e65e6a93
WojciechM <wojtekmajnert@gmail.com>
2025-04-03 08:46:39 +0200

    Added few functions and improved code clarity

--- main.py
+++ main.py
@@ -8,6 +8,30 @@ from pwhshr import Password, PasswordHash
 from schemas import *
 import security as sc
 
+def find_user_in_db(user_id: int, instance: Session) -> User:
+    """Check if user with user_id exist in database and return user. 
+    If user doesn't exist then raise HTTPException"""
+    user = instance.execute(db.select(db.UserT).where(db.UserT.user_id == user_id)).scalar()
+    if user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+    return user
+
+def find_task_in_db(task_id: int, instance: Session) -> Task:
+    """Check if task with task_id exist in database and return task.
+    If task doesn't exist then raise HTTPException"""
+    task = instance.execute(db.select(db.TaskT).where(db.TaskT.task_id == task_id)).scalar()
+    if task is None:
+        raise HTTPException(status_code=404, detail='Task not found')
+    return task
+
+def find_comment_in_db(comment_id: int, instance: Session) -> Comment:
+    """Check if comment with comment_id exist in database and return comment.
+    If comment doesn't exist then raise HTTPException"""
+    comment = instance.execute(db.select(db.CommentT).where(db.CommentT.comment_id == comment_id)).scalar()
+    if comment is None:
+        raise HTTPException(status_code=404, detail='Comment not found')
+    return comment
+
 app = FastAPI()
 auth = APIRouter(dependencies=[Depends(sc.get_current_user)])
 
@@ -31,24 +55,21 @@ def get_users(instance: Session = Depends(db.get_session)):
 @auth.get('/users/{user_id}', response_model=User)
 def get_user(user_id: int, instance: Session = Depends(db.get_session)):
     """Find user by passing user_id"""
-    user = instance.execute(db.select(db.UserT).where(db.UserT.user_id == user_id)).scalar()
-    if user is None:
-        raise HTTPException(status_code=404, detail='User not found. Operation rejected.')
+    user = find_user_in_db(user_id, instance=instance)
     return user
 
-@auth.post('/users/add', response_model=User)
+@app.post('/users/add', response_model=User)
 def create_user(username: str, email: str, password: str, instance: Session = Depends(db.get_session)):
     """Create new user db.UserT class by passing UserCreate variables"""
     new_user = db.UserT(username, email, password)
     instance.add(new_user)
     instance.commit()
-    instance.refresh(new_user)
     return new_user
 
 @auth.post('/users/update', response_model=User)
 def update_user(user_id: int, params: UserUpdate, instance: Session = Depends(db.get_session)):
     """Update user"""
-    user_to_update = instance.execute(db.select(db.UserT).where(db.UserT.user_id == user_id)).scalar()
+    user_to_update = find_user_in_db(user_id, instance=instance)
     # Updating user information with model_dump() and setattr()
     update_data = params.model_dump(exclude_unset=True)
     [setattr(user_to_update, key, value) for key, value in update_data.items()]
@@ -58,12 +79,9 @@ def update_user(user_id: int, params: UserUpdate, instance: Session = Depends(db
 @auth.delete('/users/delete/{user_id}', response_model=UserDelete)
 def delete_user(user_id: int, instance: Session = Depends(db.get_session)):
     """Delete user by entering his ID and tasks that he owns"""
-    user_to_del = instance.execute(db.select(db.UserT).where(db.UserT.user_id == user_id)).scalar()
-    if user_to_del is not None:
-        instance.delete(user_to_del)
-        instance.commit()
-    else:
-        raise HTTPException(status_code=404, detail='User not found. Operation rejected.')
+    user_to_del = find_user_in_db(user_id, instance=instance)
+    instance.delete(user_to_del)
+    instance.commit()
     return user_to_del
 
 
@@ -73,16 +91,13 @@ def delete_user(user_id: int, instance: Session = Depends(db.get_session)):
 def get_tasks(instance: Session = Depends(db.get_session)):
     """Return all tasks"""
     tasks = instance.execute(db.select(db.TaskT)).unique().scalars()
-    # Task list comprehension
     result = [task for task in tasks]
     return result
 
 @auth.get('/tasks/{task_id}', response_model=Task)
 def get_task(task_id: int, instance: Session = Depends(db.get_session)):
     """Return one task by entering task ID"""
-    task = instance.execute(db.select(db.TaskT).where(db.TaskT.task_id == task_id)).scalar()
-    if task is None:
-        raise HTTPException(status_code=404, detail='Task not found. Operation rejected.')
+    task = find_task_in_db(task_id, instance=instance)
     return task
 
 @auth.post('/tasks/add', response_model=Task)
@@ -93,17 +108,23 @@ def add_task(user_id: int, title: str, description: str, instance: Session = Dep
     instance.commit()
     return new_task
 
+@auth.post('/tasks/assign', response_model=Task)
+def assign_user_to_task(owner_id: int, task_id: int, user_id_to_assign: int, instance: Session = Depends(db.get_session)):
+    """Assign user to task"""
+    find_user_in_db(owner_id, instance=instance)
+    user_to_assign = find_user_in_db(user_id_to_assign, instance=instance)
+    task = find_task_in_db(task_id, instance=instance)
+    task.assigned_users.append(user_to_assign)
+    instance.commit()
+    return task
+
 @auth.post('/tasks/update', response_model=Task)
-def update_task(user_id: int, task_id: int, params: TaskUpdate, instance: Session = Depends(db.get_session)):
+def update_task(owner_id: int, task_id: int, params: TaskUpdate, instance: Session = Depends(db.get_session)):
     """Update task"""
-    user = instance.execute(db.select(db.UserT).where(db.UserT.user_id == user_id)).scalar()
-    if user is None: # Check is user exist
-        raise HTTPException(status_code=404, detail='User not found. Operation rejected.')
-    task_to_update = instance.execute(db.select(db.TaskT).where(db.TaskT.task_id == task_id)).scalar()
-    if task_to_update is None: # Check if task exist
-        raise HTTPException(status_code=404, detail='Task not found. Operation rejected.')
-    is_user_owner = True if user_id == task_to_update.owner_id else None
-    if is_user_owner is None:
+    find_user_in_db(owner_id, instance=instance)
+    task_to_update = find_task_in_db(task_id, instance=instance)
+    is_user_owner = True if owner_id == task_to_update.owner_id else False
+    if is_user_owner is False:
         raise HTTPException(status_code=403, detail='Permission denied. This user does not have permission to update this task.')
     else:
         # Updating user information with model_dump() and setattr()
@@ -115,12 +136,9 @@ def update_task(user_id: int, task_id: int, params: TaskUpdate, instance: Sessio
 @auth.delete('/tasks/delete/{task_id}', response_model=TaskDelete)
 def delete_task(task_id: int, instance: Session = Depends(db.get_session)):
     """Delete task"""
-    task_to_del = instance.execute(db.select(db.TaskT).where(db.TaskT.task_id == task_id)).scalar()
-    if task_to_del is not None:
-        instance.delete(task_to_del)
-        instance.commit()
-    else:
-        raise HTTPException(status_code=404, detail='User not found. Operation rejected.')
+    task_to_del = find_task_in_db(task_id, instance=instance)
+    instance.delete(task_to_del)
+    instance.commit()
     return task_to_del
 
 
@@ -129,54 +147,38 @@ def delete_task(task_id: int, instance: Session = Depends(db.get_session)):
 @auth.post('/tasks/comments/add', response_model=Comment)
 def add_comment(user_id: int, task_id: int, comment: str, instance: Session = Depends(db.get_session)):
     """Add new comment in specific task"""
-    user = instance.execute(db.select(db.UserT).where(db.UserT.user_id == user_id)).scalar()
-    if user is None: # Checking if user exist
-        raise HTTPException(status_code=404, detail='User not found. Operation rejected.')
-    task = instance.execute(db.select(db.TaskT).where(db.TaskT.task_id == task_id)).scalar()
-    if task is None: # Checking if task exist
-        raise HTTPException(status_code=404, detail='Task not found. Operation rejected.')
+    user = find_user_in_db(user_id, instance=instance)
+    task = find_task_in_db(task_id, instance=instance)
     # Checking if user owns or is assigned to task
     is_assigned = [t.task_id for t in user.assigned_tasks if t.task_id == task.task_id]
     is_owned = [t.task_id for t in user.owned_tasks if t.task_id == task.task_id]
     if not (is_owned or is_assigned):
         raise HTTPException(status_code=403, detail='Permission denied. This user does not have permission to comment this task.')
-    else:
-        # Creating new comment, appending it to relationships list in task.comments finally commit
-        new_comment = db.CommentT(user_id, task_id, comment)
-        task.comments.append(new_comment)
-        instance.add(task)
-        instance.commit()
+    # Creating new comment, appending it to relationships list in task.comments finally commit
+    new_comment = db.CommentT(user_id, task_id, comment)
+    task.comments.append(new_comment)
+    instance.add(task)
+    instance.commit()
     return new_comment
 
 @auth.post('/tasks/comments/update', response_model=Comment)
 def update_comment(user_id: int, comment_id: int, params: CommentUpdate, instance: Session = Depends(db.get_session)):
     """Update comment"""
-    user = instance.execute(db.select(db.UserT).where(db.UserT.user_id == user_id)).scalar()
-    if user is None: # Checking if user exist
-        raise HTTPException(status_code=404, detail='User not found. Operation rejected.')
-    comment_to_update = instance.execute(db.select(db.CommentT).where(db.CommentT.comment_id == comment_id)).scalar()
-    if comment_to_update is None: # Checking if user exist
-        raise HTTPException(status_code=404, detail='Comment not found. Operation rejected.')
+    find_user_in_db(user_id, instance=instance)
+    comment_to_update = find_comment_in_db(comment_id, instance=instance)
     if not user_id == comment_to_update.user_id:
         raise HTTPException(status_code=403, detail='Permission denied. This user does not have permission to update this comment.')
-    else:
-        update_data = params.model_dump(exclude_unset=True)
-        [setattr(comment_to_update, key, value) for key, value in update_data.items()]
-        instance.commit()
+    update_data = params.model_dump(exclude_unset=True)
+    [setattr(comment_to_update, key, value) for key, value in update_data.items()]
+    instance.commit()
     return comment_to_update
 
 @auth.delete('/tasks/comments/delete', response_model=CommentDelete)
 def delete_comment(user_id: int, comment_id: int, instance: Session = Depends(db.get_session)):
     """Delete comment"""
     # Check if user exist
-    user = instance.execute(db.select(db.UserT).where(db.UserT.user_id == user_id)).scalar()
-    if user is None:
-        raise HTTPException(status_code=404, detail='User not found. Operation rejected.')
-    comment_to_del = instance.execute(db.select(db.CommentT).where(db.CommentT.comment_id == comment_id)).scalar()
-    # Check if comment exist
-    if comment_to_del is None:
-        raise HTTPException(status_code=404, detail='Comment not found. Operation rejected.')
-    
+    user = find_user_in_db(user_id, instance=instance)
+    comment_to_del = find_comment_in_db(comment_id, instance=instance)
     instance.delete(comment_to_del)
     instance.commit()
     return comment_to_del
--- schemas.py
+++ schemas.py
@@ -12,20 +12,18 @@ class UserBase(BaseModel):
     username: str = Field(..., min_length=3, max_length=512, description='Name of a user')
     email: Optional[str] = Field(None, description='User email adress')
 
-class User(UserBase):
+class UserWithID(UserBase):
+    user_id: int = Field(..., description='ID of the user')
+    
+class User(UserWithID):
     class AssignedTasks(BaseModel):
         task_id: int = Field(..., description='ID of a task')
     
     class OwnedTasks(BaseModel):
         task_id: int = Field(..., description='ID of a task') 
 
-    user_id: int = Field(..., description='ID of the user')
     owned_tasks: Optional[List[OwnedTasks]] = Field(None, description='Tasks owned by user')
     assigned_tasks: Optional[List[AssignedTasks]] = Field(None, description='Tasks assigned to user')
-
-class UserLogin(BaseModel):
-    user_id: int = Field(..., description='ID of the user')
-    logged: bool = Field(default=False, description='Information about succesfull loggin')
 
 
 class UserCreate(UserBase):
@@ -56,10 +54,13 @@ class Task(TaskBase):
                                                 'The default start date is the date task was created')
     end_date: Optional[datetime.date] = Field(None, description='Task end date')
     comments: Optional[List['Comment']] = Field(None, description='Task comments created by users')
-    assigned_users: Optional[List['User']] = Field(None, description='Users assigned to the task')
+    assigned_users: Optional[List['UserWithID']] = Field(None, description='Users assigned to the task')
 
 class TaskAdd(TaskBase):
     pass
+
+class TaskAssign(BaseModel):
+    user_id: int = Field(..., description='ID of a assigned user')
 
 class TaskUpdate(BaseModel):
     title: Optional[str] = Field(None, min_length=5, max_length=512, description='Title of a task')
@@ -68,7 +69,7 @@ class TaskUpdate(BaseModel):
     start_date: Optional[datetime.date] = Field(None, description='Task start date. '
                                                 'The default start date is the date task was created')
     end_date: Optional[datetime.date] = Field(None, description='Task end date')
-    assigned_users: Optional[List['User']] = Field(None, description='Users assigned to the task')
+    #assigned_users: Optional[List['User']] = Field(None, description='Users assigned to the task')
 
 class TaskDelete(Task):
     detail: str = Field(default='The task deletion operation has been performed successfully. '
@@ -78,7 +79,7 @@ class TaskDelete(Task):
 # Comment table models
 
 class CommentBase(BaseModel):
-    task_id: int = Field(..., description='ID of a parent task')
+    # task_id: int = Field(..., description='ID of a parent task') # No need to show this info 
     user_id: int = Field(..., description='ID of a user')
     timestamp: datetime.datetime = Field(..., description='Date and time when comment was added')
     comment: str = Field(..., description='Comment body')
